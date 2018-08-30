202b40e91de34705fb559c3b45604bc8d226b58a
Georgi Yankov <gyankov@progress.com>
2018-08-30 16:44:06 +0300

    chore: added custom datasource sample

---
+++ grid/custom-data-source/src/WebApplication3/Models/MyResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+namespace WebApplication3.Models
+{
+    public class MyResponseModel
+    {
+        public IEnumerable DataCollection{ get; set; }
+
+        public int TotalRecords { get; set; }
+    }
+}
