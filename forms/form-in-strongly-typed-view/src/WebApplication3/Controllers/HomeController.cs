46dbb586337f9190d3cedabdc778c4873a848e65
Georgi Yankov <gyankov@progress.com>
2018-09-10 17:47:12 +0300

    chore: added a form in strongly typed view sample

---
+++ forms/form-in-strongly-typed-view/src/WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {           
+            return View(new Product { Discontinued = true, ProductID = 1, ProductName = "Chai", UnitPrice = 18, UnitsInStock = 39 });
+        }
+
+        public IActionResult Data(Product product)
+        {
+            return Json(product);
+        }
+
+    }
+}
