a028207e83ae3f15640b0248fb094717dcb144de
Georgi Yankov <gyankov@progress.com>
2018-11-05 17:09:07 +0200

    chore: added enabled operations for object column sample /telerik/kendo#8450

---
+++ grid/enable-operations-for-object-column/src/WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static ICollection<Product> products;
+        private static ICollection<Category> categories;
+
+        public HomeController()
+        {
+            if (products == null)
+            {
+                var random = new Random();
+                products = Enumerable.Range(1, 100).Select(x => new Product
+                {
+                    Discontinued = x % 2 == 1,
+                    ProductID = x,
+                    ProductName = "Product " + x,
+                    UnitPrice = random.Next(1, 1000),
+                    UnitsInStock = random.Next(1, 1000),
+                    UnitsOnOrder = random.Next(1, 1000),
+                    Category = new Category
+                    {
+                        CategoryID = x,
+                        CategoryName = "Category" + x
+                    }
+                    
+                }).ToList();
+
+                categories = Enumerable.Range(1, 100).Select(x => new Category
+                {
+                        CategoryID = x,
+                        CategoryName = "Category" + x                    
+
+                }).ToList();
+            }
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+
+        public ActionResult Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(products.ToDataSourceResult(request));
+        }
+
+        public ActionResult AllCategories()
+        {
+            return Json(categories);
+        }
+
+
+        [AcceptVerbs("Post")]
+        public ActionResult Create([DataSourceRequest] DataSourceRequest request, Product product)
+        {
+            //save item to database
+            return Json(new[] { product }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs("Post")]
+        public ActionResult Update([DataSourceRequest] DataSourceRequest request, Product product)
+        {
+            //save item to database
+            products.Add(product);
+            return Json(new[] { product }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs("Post")]
+        public ActionResult Destroy([DataSourceRequest] DataSourceRequest request, Product product)
+        {
+            //remove item from database
+            products.Remove(product);
+            return Json(new[] { product }.ToDataSourceResult(request, ModelState));
+        }
+
+       
+
+
+
+    }
+}
