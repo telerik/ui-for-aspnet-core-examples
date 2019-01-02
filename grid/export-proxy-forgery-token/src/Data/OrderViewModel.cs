02b1e6f12c296c3bdba9a9415d2e4abc9542587e
Tsvetomir Milchev <tmilchev@progress.com>
2019-01-02 17:35:29 +0200

    chore: add proxy export with forgery token sample

---
+++ grid/export-proxy-forgery-token/src/Data/OrderViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GridRazorPagesApp.Data
+{
+    public class OrderViewModel
+    {
+        public int OrderID
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public decimal? Freight
+        {
+            get;
+            set;
+        }
+
+        public DateTime? OrderDate
+        {
+            get;
+            set;
+        }
+
+        public string ShipCity
+        {
+            get;
+            set;
+        }
+
+        public string ShipName
+        {
+            get;
+            set;
+        }
+    }
+}
