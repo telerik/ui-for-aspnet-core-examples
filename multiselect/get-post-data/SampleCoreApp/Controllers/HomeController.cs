2a587567602949acac1c637d977ac559467af69f
Marin Bratanov <m.bratanov@gmail.com>
2019-01-05 10:34:53 +0200

    chore: update multiselect post example so it runs

---
+++ multiselect/get-post-data/SampleCoreApp/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SampleCoreApp.Models;
+
+namespace SampleCoreApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+	}
+}
