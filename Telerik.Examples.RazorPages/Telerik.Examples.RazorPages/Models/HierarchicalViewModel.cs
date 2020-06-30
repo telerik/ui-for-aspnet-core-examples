96d132f9675ed401f6d773f5100ddc1b6a14fa72
aevangel <aleksandar.evangelatov@progress.com>
2020-06-30 17:52:45 +0300

    feat: add TreeView example for RazorPages

---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Models/HierarchicalViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Telerik.Examples.RazorPages.Models
+{
+    public class HierarchicalViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int? ParentID { get; set; }
+        public bool HasChildren { get; set; }
+    }
+}
---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Pages/TreeView/TreeViewIndex.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Telerik.Examples.RazorPages.Models;
+
+namespace Telerik.Examples.RazorPages.Pages.TreeView
+{
+    public class TreeViewIndexModel : PageModel
+    {
+        public static IList<HierarchicalViewModel> result = new List<HierarchicalViewModel>()
+        {
+            new HierarchicalViewModel() { ID = 1, ParentID = null, HasChildren = true, Name = "Parent Item 1" },
+            new HierarchicalViewModel() { ID = 2, ParentID = null, HasChildren = true, Name = "Parent Item 2" },
+            new HierarchicalViewModel() { ID = 3, ParentID = null, HasChildren = true, Name = "Parent Item 3" },
+            new HierarchicalViewModel() { ID = 4, ParentID = 1, HasChildren = false, Name = "Child Item 1" },
+            new HierarchicalViewModel() { ID = 5, ParentID = 1, HasChildren = false, Name = "Child Item 2" },
+            new HierarchicalViewModel() { ID = 6, ParentID = 2, HasChildren = false, Name = "Child Item 3" },
+            new HierarchicalViewModel() { ID = 7, ParentID = 2, HasChildren = false, Name = "Child Item 4" },
+            new HierarchicalViewModel() { ID = 8, ParentID = 3, HasChildren = false, Name = "Child Item 5" },
+            new HierarchicalViewModel() { ID = 9, ParentID = 3, HasChildren = false, Name = "Child Item 6" }
+        };
+
+        public JsonResult OnGetTreeViewRead(int? id)
+        {
+            var data = result.Where(x => id.HasValue ? x.ParentID == id : x.ParentID == null)
+                .Select(item => new {
+                    id = item.ID,
+                    Name = item.Name,
+                    hasChildren = item.HasChildren
+                });
+
+            return new JsonResult(data);
+        }
+    }
+}
