382433832eb125e9858a3027830cfefd0e111245
DESKTOP-M5A0PQU\Neli <kondovaneli@gmail.com>
2020-08-19 18:35:58 +0300

    add DropDownTree MultiColumnComboBox and MultiSelect examples

---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Pages/MultiColumnComboBox/MultiColumnComboBoxIndex.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Telerik.Examples.RazorPages.Models;
+
+namespace Telerik.Examples.RazorPages.Pages.MultiColumnComboBox
+{
+    public class MultiColumnComboBoxIndexModel : PageModel
+    {
+        public static List<OrderViewModel> orders;
+        public void OnGet()
+        {
+            if (orders == null)
+            {
+                orders = new List<OrderViewModel>();
+
+                Enumerable.Range(1, 50).ToList().ForEach(i => orders.Add(new OrderViewModel
+                {
+                    OrderID = i + 1,
+                    Freight = i * 10,
+                    ShipName = "ship name " + i,
+                    ShipCity = "ship city " + i
+                }));
+
+            }
+        }
+
+        public JsonResult OnGetRead(string filterValue)
+        {
+            if (filterValue != null)
+            {
+                var filteredData = orders.Where(p => p.ShipName.Contains(filterValue));
+                return new JsonResult(filteredData);
+            }
+            return new JsonResult(orders);
+        }
+    }
+}
---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Pages/MultiSelect/MultiSelectIndex.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Telerik.Examples.RazorPages.Models;
+
+namespace Telerik.Examples.RazorPages.Pages.MultiSelect
+{
+    public class MultiSelectIndexModel : PageModel
+    {
+        public static List<Customer> customers;
+        public void OnGet()
+        {
+            if (customers == null)
+            {
+                customers = new List<Customer>();
+
+                Enumerable.Range(1, 50).ToList().ForEach(i => customers.Add(new Customer
+                {
+                    CustomerId = i,
+                    Name = "Customer Name " + i,
+                    Address = "city " + i
+                }));
+
+            }
+        }
+
+        public JsonResult OnGetRead()
+        {            
+            var filteredData = customers;
+            return new JsonResult(filteredData);
+        }
+    }
+}
