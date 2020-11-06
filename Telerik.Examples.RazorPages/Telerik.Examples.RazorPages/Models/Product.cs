3c6524d3a5157b7757164453bbaab21eeeebb66a
DESKTOP-M5A0PQU\Neli <kondovaneli@gmail.com>
2020-11-06 09:40:59 +0200

    feat: add Menu ContextMenu and Button examples

---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Telerik.Examples.RazorPages.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public int CategoryID { get; set; }
+        public string ProductName { get; set; }
+    }
+}
---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Pages/Button/ButtonIndex.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Telerik.Examples.RazorPages.Pages.Button
+{
+    public class ButtonIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost(string description)
+        {
+
+        }
+    }
+}
