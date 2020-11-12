d382baa374fb2676f47c72e764203dad94e3e3e5
Neli Kondova <kondovaneli@gmail.com>
2020-11-12 11:43:03 +0200

    add Dialog example (#23)

---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Pages/Dialog/DialogIndex.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Telerik.Examples.RazorPages.Pages.Dialog
+{
+    public class DialogIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost(string custom)
+        {
+
+        }
+    }
+}
