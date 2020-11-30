6091b4d82e1ce9035fb1a2040e58320b2f349b74
IvanDanchev <ifujin@gmail.com>
2020-11-30 10:13:20 +0200

    chore: add Slider Splitter and Upload examples

---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Pages/Slider/SliderIndex.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Telerik.Examples.RazorPages.Pages.Slider
+{
+    public class SliderIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Pages/Upload/UploadIndex.cshtml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Telerik.Examples.RazorPages.Pages.Upload
+{
+    public class UploadIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+		public ActionResult OnPostSave(IEnumerable<IFormFile> files)
+		{
+			// The Name of the Upload component is "files"
+			if (files != null)
+			{
+				foreach (var file in files)
+				{
+					var fileContent = ContentDispositionHeaderValue.Parse(file.ContentDisposition);
+
+					//// Some browsers send file names with full path.
+					//// We are only interested in the file name.
+					var fileName = Path.GetFileName(fileContent.FileName.Trim('"'));
+					var physicalPath = Path.Combine("App_Data", fileName);
+
+					//// The files are not actually saved in this demo
+					////file.SaveAs(physicalPath);
+				}
+			}
+
+			// Return an empty string to signify success
+			return Content("");
+		}
+
+		public ActionResult OnPostRemove(string[] fileNames)
+		{
+			// The parameter of the Remove action must be called "fileNames"
+
+			if (fileNames != null)
+			{
+				foreach (var fullName in fileNames)
+				{
+					var fileName = Path.GetFileName(fullName);
+					var physicalPath = Path.Combine("App_Data", fileName);
+
+					// TODO: Verify user permissions
+
+					if (System.IO.File.Exists(physicalPath))
+					{
+						// The files are not actually removed in this demo
+						// System.IO.File.Delete(physicalPath);
+					}
+				}
+			}
+
+			// Return an empty string to signify success
+			return Content("");
+		}
+	}
+}
