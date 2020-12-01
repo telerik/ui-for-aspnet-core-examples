b28b1160a838cb38d4a472f00c8d2c7fd8e92506
Georgi Denchev <gdenchev@progress.com>
2020-12-01 19:32:27 +0200

    feat: add MediaPlayer and Playlist examples

---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Models/Video.cs
@@ -0,0 +1,11 @@
+﻿namespace Telerik.Examples.RazorPages.Models
+{
+    public class Video
+    {
+        public string Title { get; set; }
+
+        public string Source { get; set; }
+
+        public string Poster { get; set; }
+    }
+}
