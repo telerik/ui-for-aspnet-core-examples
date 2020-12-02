0e5d4862161db11f1675e6689f75e3999ed62986
Georgi Denchev <gdenchev@progress.com>
2020-12-02 19:11:28 +0200

    feat: add TreeList CRUD example

---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Models/EmployeeDirectoryModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Telerik.Examples.RazorPages.Models
+{
+    public class EmployeeDirectoryModel
+    {
+        [ScaffoldColumn(false)]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int? ReportsTo { get; set; }
+
+        public string Position { get; set; }
+
+        [ScaffoldColumn(false)]
+        public bool hasChildren { get; set; }
+
+        private DateTime? hireDate;
+        [DataType(DataType.Date)]
+        [Display(Name = "Hire Date")]
+        public DateTime? HireDate
+        {
+            get
+            {
+                return hireDate;
+            }
+            set
+            {
+                if (value.HasValue)
+                {
+                    hireDate = value.Value;
+                }
+                else
+                {
+                    hireDate = null;
+                }
+            }
+        }
+    }
+}
