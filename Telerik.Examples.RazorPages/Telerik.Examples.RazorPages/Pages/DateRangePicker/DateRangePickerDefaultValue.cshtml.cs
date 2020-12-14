c698655fcce93b6fed644ca0d07b05a62d84b90d
Georgi Denchev <gdenchev@progress.com>
2020-12-14 18:18:10 +0200

    feat: add DateRangePicker examples

---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Pages/DateRangePicker/DateRangePickerDefaultValue.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Telerik.Examples.RazorPages.Pages.DateRangePicker
+{
+    public class DateRangePickerDefaultValueModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
