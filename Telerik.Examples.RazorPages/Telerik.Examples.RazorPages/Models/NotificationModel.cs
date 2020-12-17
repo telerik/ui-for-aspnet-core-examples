07eb2ee2c91c5a07da256cd4fc14c74c2445f7d7
Neli Kondova <kondovaneli@gmail.com>
2020-12-17 15:21:18 +0200

    chore(demos): modify Notification and Window examples (#26)

---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Models/NotificationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Telerik.Examples.RazorPages.Models
+{
+    public class NotificationModel
+    {
+        public string Text { get; set;  }
+        public DateTime Time { get; set; }
+    }
+}
--- Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Pages/Window/WindowIndex.cshtml.cs
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Pages/Window/WindowIndex.cshtml.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Telerik.Examples.RazorPages.Models;
 
 namespace Telerik.Examples.RazorPages.Pages.Window
 {
     public class WindowIndexModel : PageModel
     {
+        public string Text = String.Empty;
         public void OnGet()
         {
-
+            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi facilisis, tortor in imperdiet iaculis, lacus sem placerat mi, eu pellentesque lectus nisl a tortor. Praesent efficitur ipsum quis neque vestibulum, id condimentum nisl bibendum. Nulla dictum efficitur eros id volutpat. Nam tellus sem, condimentum at lacinia eu, porta a mi. Fusce varius suscipit ullamcorper. Sed ac luctus tellus. Aenean efficitur purus pellentesque, accumsan mi ut, porttitor purus. Nulla efficitur, lacus quis pellentesque pellentesque, neque enim iaculis ligula, vel volutpat mi odio id nibh. Suspendisse laoreet commodo magna vitae tempus. Curabitur vitae massa tortor. In nisi turpis, tristique vitae volutpat in, iaculis non lacus. Morbi quis augue risus. Mauris porttitor, sem ut cursus pretium, diam turpis vulputate eros, vitae ullamcorper arcu lacus vel libero.";
         }
     }
 }
