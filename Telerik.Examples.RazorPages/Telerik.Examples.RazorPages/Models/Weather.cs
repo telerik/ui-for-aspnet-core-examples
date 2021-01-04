60a8fac22e290587d3ee50f41df817723b224cbd
Nikolay Penev <npenev@progress.com>
2021-01-04 13:41:27 +0200

    chore(sparkline): add sparkline remote binding razorpage demo
    telerik/kendo/issues/11757

---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Models/Weather.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Telerik.Examples.RazorPages.Models
+{
+    public class Weather
+    {
+        public int Id { get; set; }
+        public double Rain { get; set; }
+
+        public double TMax { get; set; }
+        public double Wind { get; set; }
+    }
+}
