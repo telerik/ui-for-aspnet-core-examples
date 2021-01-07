47e6e4e9ed7eb48cd7a779f5f03a1309496805aa
aevangel <aleksandar.evangelatov@progress.com>
2021-01-07 16:24:09 +0200

    chore(demos): add ImageEditor RazorPage demo

---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Pages/ImageEditor/ImageEditorIndex.cshtml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Telerik.Examples.RazorPages.Pages.ImageEditor
+{
+    public class ImageEditorIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPostSave(string contentType, string base64, string fileName)
+        {
+            var fileContents = Convert.FromBase64String(base64);
+
+            return File(fileContents, contentType, fileName);
+        }
+    }
+}
