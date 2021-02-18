f7307721f4e28f1e0f823d96a10af459e731a9f1
Neli Kondova <kondovaneli@gmail.com>
2021-02-18 16:46:31 +0200

    chore(demos): add wizard ajax submit example (#38)

---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Models/UserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Telerik.Examples.RazorPages.Models
+{
+    public class UserViewModel
+    {
+        [Required]
+        public long UserId { get; set; }
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Name must be at least 5 characters long")]
+        public string Name { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string BirthDate { get; set; }
+
+        public string Gender { get; set; }
+    }
+}
