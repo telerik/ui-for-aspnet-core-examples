976066170682a3556a39604986d5db8e757df7e1
aevangel <aleksandar.evangelatov@progress.com>
2021-02-18 09:32:38 +0200

    chore(demos): add Tooltip RazorPage demo

---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Pages/Tooltip/TooltipIndex.cshtml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Telerik.Examples.RazorPages.Models;
+
+namespace Telerik.Examples.RazorPages.Pages.Tooltip
+{
+    public class TooltipIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
