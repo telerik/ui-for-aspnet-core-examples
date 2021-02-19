13915a9cb257b9c4ac41b76ad8dc0fbbe8134922
aevangel <aleksandar.evangelatov@progress.com>
2021-02-19 16:56:47 +0200

    chode(demos): add DropDownList Virtualization example

---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Pages/DropDownList/DropDownListVirtualization.cshtml.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Telerik.Examples.RazorPages.Models;
+
+namespace Telerik.Examples.RazorPages.Pages.DropDownList
+{
+    public class DropDownListVirtualizationModel : PageModel
+    {
+        public static IList<OrderViewModel> orders;
+
+        public void OnGet()
+        {
+            if (orders == null)
+            {
+                orders = new List<OrderViewModel>();
+
+                Enumerable.Range(1, 5000).ToList().ForEach(i => orders.Add(new OrderViewModel
+                {
+                    OrderID = i + 1,
+                    Freight = i * 10,
+                    OrderDate = DateTime.Now.AddDays(i % 7),
+                    ShipName = "ShipName " + i,
+                    ShipCity = "ShipCity " + i
+                }));
+
+            }
+        }
+
+        public JsonResult OnPostRead([DataSourceRequest] DataSourceRequest request)
+        {
+            return new JsonResult(orders.ToDataSourceResult(request));
+        }
+
+        public JsonResult OnPostValueMapper(int[] values)
+        {
+            var indices = new List<int>();
+            if (values != null && values.Any())
+            {
+                var index = 0;
+                foreach (var order in orders)
+                {
+                    if (values.Contains(order.OrderID))
+                    {
+                        indices.Add(index);
+                    }
+                    index += 1;
+                }
+            }
+
+            return new JsonResult(indices);
+        }
+    }
+}
