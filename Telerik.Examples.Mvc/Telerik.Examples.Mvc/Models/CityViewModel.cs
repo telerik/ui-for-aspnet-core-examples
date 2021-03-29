f97d9b1fb2491007de6cd6b1f5944f936e4db8dc
Nikolay Penev <npenev@progress.com>
2021-03-29 17:23:37 +0300

    feat(grid): add example with encoded foreign key values
    telerik/kendo/issues/11022

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Models/CityViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Telerik.Examples.Mvc.Models
+{
+    public class CityViewModel
+    {
+        public int? CityID { get; set; }
+        public string CityName { get; set; }
+    }
+}
