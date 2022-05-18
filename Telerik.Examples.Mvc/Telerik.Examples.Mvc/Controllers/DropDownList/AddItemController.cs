1af899a76e431880876c9ff5b7fdbd0588dd97b8
alestoya <alestoya@progress.com>
2022-05-18 20:20:20 +0300

    chore(demos): add DropDownList Add Item example

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Controllers/DropDownList/AddItemController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Telerik.Examples.Mvc.Models;
+
+namespace Telerik.Examples.Mvc.Controllers.DropDownList
+{
+    public class AddItemController : Controller
+    {
+        public IActionResult AddItem()
+        {
+            return View("~/Views/DropDownList/AddItem.cshtml");
+        }
+        public IActionResult GetLocations()
+        {
+            IEnumerable<Location> locations = LocationsData();
+
+            return Json(locations);
+        }
+        public IActionResult CreateLocation(Location location)
+        {
+            return Json(new[] { location });
+        }
+        private List<Location> LocationsData()
+        {
+            return new List<Location>()
+            {
+                new Location() { Id = 1, Name = "London" },
+                new Location() { Id = 2, Name = "Paris" },
+                new Location() { Id = 3, Name = "Sofia" }
+            };
+        }
+    }
+}
