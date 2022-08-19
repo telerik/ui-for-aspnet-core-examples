d9fe30fb17553646122727baa150e26416c8aeae
Alexander Stoyanov <92315320+alestoya@users.noreply.github.com>
2022-08-19 20:29:54 +0300

    chore(demos): add Grid Audio Column example
    * chore(demos): add Grid Audio Column example
    
    * chore(demos): add CRUD operations and minor enchancements

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Models/MusicianViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Telerik.Examples.Mvc.Models
+{
+    public class MusicianViewModel
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Song Writer")]
+        public string Name { get; set; }
+
+        [UIHint("Number")]
+        public int Age { get; set; }
+
+        public string Nationality { get; set; }
+
+        [Display(Name = "Introduction")]
+        public Audio Intro { get; set; }
+    }
+
+    public class Audio
+    {
+        public string FileName { get; set; }
+        public string Extension { get; set; }
+    }
+}
