f3a3fa395548237866154bce38e45bad34d85fc8
alestoya <alestoya@progress.com>
2023-07-28 11:43:40 +0300

    chore(demos): add wizard dynamic step example

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Controllers/Wizard/DynamicStepController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telerik.Examples.Mvc.Models;
+
+namespace Telerik.Examples.Mvc.Controllers.Wizard
+{
+    public class DynamicStepController : Controller
+    {
+        public IActionResult DynamicStep()
+        {
+            return View("/Views/Wizard/DynamicStep.cshtml");
+        }
+
+        [HttpPost]
+        public IActionResult DynamicStep(UserDetailsModel model)
+        {
+            return Json(model);
+        }
+        public IActionResult Load_Step(string type)
+        {
+            return PartialView("_DynamicStepPartial", type);
+        }
+    }
+}
---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Models/UserDetailsModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Telerik.Examples.Mvc.Models
+{
+    public class UserDetailsModel
+    {
+        public string Type
+        {
+            get;
+            set;
+        }
+        public AccountDetailsModel AccountDetails
+        {
+            get;
+            set;
+        }
+
+        public CompanyDetailsModel CompanyDetails
+        {
+            get;
+            set;
+        }
+    }
+    public class AccountDetailsModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+
+    public class CompanyDetailsModel
+    {
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public string CompanyName { get; set; }
+
+    }
+}
