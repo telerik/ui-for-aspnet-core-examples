41b0d977aebf331d67c3f38bfb47ccd3be569520
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2023-08-22 19:45:06 +0300

    chore: add a project showcasing the CSP Compliance examples

---
+++ Telerik.Examples.ContentSecurityPolicy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Telerik.Examples.ContentSecurityPolicy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
