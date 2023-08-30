9b3f824ad01c31a9ac116ef0bd14760b4dce3d8f
alestoya <alestoya@progress.com>
2023-08-30 08:47:13 +0300

    chore(demos): add grid cascading foreign keys razor pages example

---
+++ Telerik.Examples.RazorPages/Telerik.Examples.RazorPages/Models/CountryModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Telerik.Examples.RazorPages.Models
+{
+    public class CountryModel
+    {
+        public string ID { get; set; }
+        public string Description { get; set; }
+    }
+}
