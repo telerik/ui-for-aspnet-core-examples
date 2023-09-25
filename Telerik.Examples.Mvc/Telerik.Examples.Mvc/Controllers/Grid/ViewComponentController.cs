a6c9f6a9d9c2e1c930b185644e4c6202400221d7
alestoya <alestoya@progress.com>
2023-09-25 18:05:14 +0300

    chore(demos): add grid viewcomponent mvc example

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Controllers/Grid/ViewComponentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Telerik.Examples.Mvc.Models;
+
+namespace Telerik.Examples.Mvc.Controllers.Grid
+{
+    public class ViewComponentController : Controller
+    {
+        public IActionResult ViewComponent()
+        {
+            var random = new Random();
+
+            var data = Enumerable.Range(1, 10)
+                .Select(x => new Product
+                {
+                    Discontinued = x % 2 == 1,
+                    ProductID = x,
+                    ProductName = "Product " + x,
+                    UnitPrice = random.Next(1, 1000),
+                    UnitsInStock = random.Next(1, 1000),
+                    UnitsOnOrder = random.Next(1, 1000)
+
+                })
+                .ToList();
+
+            return View(data);
+        }
+    }
+}
---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Telerik.Examples.Mvc.Models;
+
+namespace Telerik.Examples.Mvc.ViewComponents
+{
+    public class GridViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(IEnumerable<Product> gridModel)
+        {
+            return View("default", gridModel);
+        }
+    }
+}
