88430896b2c8299171ab0f2cfa4ed02eda6bdea1
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2024-02-06 20:05:17 +0200

    feat: add Cascading MultiSelect example

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Controllers/MultiSelect/Cascading_MultiSelectController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Linq;
+
+namespace Telerik.Examples.Mvc.Controllers.MultiSelect
+{
+	public class Cascading_MultiSelectController : Controller
+	{
+		public IActionResult Cascading_MultiSelect()
+		{
+			return View();
+		}
+		public IActionResult ReadParent()
+		{
+			var results = Enumerable.Range(1, 10).Select(i => new SelectListItem
+			{
+				Text = "Super cool item " + i,
+				Value = i.ToString()
+			});
+			return Json(results);
+		}
+
+		public IActionResult ReadChild()
+		{
+			var selectionList = this.Request.Form.ToList().Select(item => (Int32.Parse(item.Value)));
+			var listMax = selectionList.Max(value => value);
+			var results = Enumerable.Range(1, listMax).Select(i => new SelectListItem
+			{
+				Text = "Nice item " + i,
+				Value = i.ToString()
+			});
+			return Json(results);
+		}
+	}
+}
