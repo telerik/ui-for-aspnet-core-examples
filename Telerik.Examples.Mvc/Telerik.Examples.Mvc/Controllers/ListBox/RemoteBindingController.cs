3e4c32bba28dfe0267fd88692c8ad29a2ca6b667
alestoya <alestoya@progress.com>
2024-12-30 18:10:43 +0200

    chore(demos): add remote binding listbox example

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Controllers/ListBox/RemoteBindingController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Telerik.Examples.Mvc.Database;
+
+namespace Telerik.Examples.Mvc.Controllers.ListBox
+{
+    public class RemoteBindingController : Controller
+    {
+        private readonly InMemoryDbContext _dbContext;
+
+        public RemoteBindingController(InMemoryDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult RemoteBinding()
+        {
+            return View();
+        }
+
+        public IActionResult GetEmployees()
+        {
+            var employees = _dbContext.Employees.ToList();
+            return Json(employees);
+        }
+    }
+}
---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Seeders/DataSeeder.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Telerik.Examples.Mvc.Database;
+using Telerik.Examples.Mvc.Models;
+
+namespace Telerik.Examples.Mvc.Seeders
+{
+    public class DataSeeder
+    {
+        public static void SeedListBoxItems(InMemoryDbContext dbContext)
+        {
+            if (dbContext.Employees.Any())
+            {
+                return;
+            }
+
+            var employees = new List<EmployeeViewModel>
+            {
+                new EmployeeViewModel(){ Id = 1, Name = "Steven White" },
+                new EmployeeViewModel(){ Id = 2, Name = "Nancy King" },
+                new EmployeeViewModel(){ Id = 3, Name = "Nancy Davolio" },
+                new EmployeeViewModel(){ Id = 4, Name = "Michael Leverling" },
+                new EmployeeViewModel(){ Id = 5, Name = "Andrew Callahan" },
+                new EmployeeViewModel(){ Id = 6, Name = "Michael Suyama" },
+            };
+
+            dbContext.Employees.AddRange(employees);
+            dbContext.SaveChanges();
+        }
+    }
+}
