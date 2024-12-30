b9e01665d848f218e17863bba859895db7a7ad9f
alestoya <alestoya@progress.com>
2024-12-30 18:10:43 +0200

    chore(demos): add remote binding listbox example

--- Telerik.Examples.Mvc/Telerik.Examples.Mvc/Models/EmployeeViewModel.cs
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Models/EmployeeViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace Telerik.Examples.Mvc.Models
 {
     public class EmployeeViewModel
     {
+        [Key]
         public int Id { get; set; }
 
         public string Name { get; set; }
