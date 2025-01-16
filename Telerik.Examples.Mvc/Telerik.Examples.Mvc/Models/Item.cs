11f2eba537ef921d3bc348be0aa9749a1e57879c
ivaylo-milanov <imilanov@progress.com>
2025-01-16 13:46:17 +0200

    chore(docs): add Grid Editing with Cascading DropDownLists telerik/kendo#21409

---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Models/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace Telerik.Examples.Mvc.Models
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public int VendorId { get; set; }
+    }
+}
