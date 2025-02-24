2a61f59956bf6119e4c549feb2614d8ec7e466c7
alestoya <alestoya@progress.com>
2025-02-24 15:52:27 +0200

    chore: add migration to comply with net9

--- Telerik.Examples.Mvc/Telerik.Examples.Mvc/Migrations/20250224111117_InitialUpdate.Designer.cs
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Migrations/20250224111117_InitialUpdate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Telerik.Examples.Mvc.Models;
 
@@ -11,9 +12,11 @@ using Telerik.Examples.Mvc.Models;
 namespace Telerik.Examples.Mvc.Migrations
 {
     [DbContext(typeof(GeneralDbContext))]
-    partial class GeneralDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250224111117_InitialUpdate")]
+    partial class InitialUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Telerik.Examples.Mvc/Telerik.Examples.Mvc/Migrations/20250224111117_InitialUpdate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Telerik.Examples.Mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RoomID",
+                table: "Tasks",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoomID",
+                table: "Tasks");
+        }
+    }
+}
